6013fb9688541f3443bb97f4e8a53cebe5cc5349
arr199 <abieledelso@gmail.com>
2024-07-10 20:27:42 +0100

    import sidebar

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,11 +1,13 @@
+import SideBar from "@/components/sideBar";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <main>
-      SIDEBAR
+    <main className="flex flex-col">
+      <SideBar></SideBar>
       {children}
     </main>
   );
