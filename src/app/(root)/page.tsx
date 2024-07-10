6086e2652bd874975b07f03b99235f78a0d96d3b
arr199 <abieledelso@gmail.com>
2024-07-10 20:28:59 +0100

    create home page welcome header and account balance information

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,5 +1,22 @@
-import Image from "next/image";
+import { AmountChart } from "@/components/home/amountChart";
+import { MoneyAmount } from "@/components/home/moneyAmount";
+import Wrapper from "@/components/shared/wrapper";
 
 export default function Home() {
-  return <>HOME PAGE</>;
+  return (
+    <Wrapper>
+      <header>
+        <h1 className="text-2xl font-bold">
+          Welcome <span className="text-blue-500">Adrian</span>
+        </h1>
+        <p className="mt-2">
+          Access and manage your account and transactions efficiently
+        </p>
+      </header>
+      <section className="flex mt-10 gap-2">
+        <AmountChart />
+        <MoneyAmount totalAccounts={1} totalAmount={1200} />
+      </section>
+    </Wrapper>
+  );
 }
