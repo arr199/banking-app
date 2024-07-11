f48a1f8d5a162ca9f8584226c16292d3825d8249
arr199 <abieledelso@gmail.com>
2024-07-11 19:42:53 +0100

    refactor: Update AmountChart component with new data and chart libraries

--- src/components/home/amountChart.tsx
+++ src/components/home/amountChart.tsx
@@ -1,3 +1,48 @@
-export function AmountChart(): JSX.Element {
-  return <>chart</>;
+"use client";
+import { ArcElement, Chart, ChartData, Legend, Tooltip } from "chart.js";
+import { Doughnut } from "react-chartjs-2";
+
+Chart.register(ArcElement, Tooltip, Legend);
+interface AmountChartProps {
+  data: AccountsData[];
+}
+interface AccountsData {
+  id: number;
+  name: string;
+  amount: number;
+}
+
+export function AmountChart({ data = [] }: AmountChartProps): JSX.Element {
+  const chartData: ChartData = {
+    datasets: [
+      {
+        label: "Banks",
+        data: data.map((e) => e.amount),
+        backgroundColor: ["#0044FF", "#00aaFF", "#00CCFF"],
+      },
+    ],
+    labels: data.map((e) => e.name),
+  };
+
+  return (
+    <div className="h-full self-center">
+      <Doughnut width="100px" height="100px"
+        title="Banks"
+        options={{
+            maintainAspectRatio : false,
+            plugins: {
+            legend: {
+              display: false,
+              align: "start",
+             
+            },
+            title: {
+              display: true,
+            },
+          },
+        }}
+        data={chartData as any}
+      />
+    </div>
+  );
 }
