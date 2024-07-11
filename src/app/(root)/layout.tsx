2556c464a6a665e9a8e2a59047a731ff31e5c39e
arr199 <abieledelso@gmail.com>
2024-07-11 18:38:43 +0100

    Update layout component with new navbar components

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,13 +1,16 @@
-import SideBar from "@/components/sideBar";
+import { LeftNavbar } from "@/components/home/leftNavbar";
+import { MobileNavbar } from "@/components/home/mobileNavbar";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const user: User = { id: 1, name: "John Doe" };
   return (
-    <main className="flex flex-col">
-      <SideBar></SideBar>
+    <main className="flex">
+      <MobileNavbar user={user}></MobileNavbar>
+      <LeftNavbar user={user}></LeftNavbar>
       {children}
     </main>
   );
