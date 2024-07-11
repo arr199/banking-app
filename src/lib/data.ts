3a152f0ac52d21b350566236cc4f93f5a737ccfa
arr199 <abieledelso@gmail.com>
2024-07-11 19:43:36 +0100

    feat: Add accountsData to store bank account information

---
+++ src/lib/data.ts
@@ -0,0 +1,17 @@
+export const accountsData = [
+  {
+    id: 1,
+    name: "Checking",
+    amount: 1235,
+  },
+  {
+    id: 2,
+    name: "Savings",
+    amount: 5000,
+  },
+  {
+    id: 3,
+    name: "Investment",
+    amount: 10000,
+  },
+];
