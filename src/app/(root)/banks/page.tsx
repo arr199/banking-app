84380c4c488d70cd8f4bf6472849d946173a47cc
arr199 <abieledelso@gmail.com>
2024-07-11 19:42:32 +0100

    create new pages

---
+++ src/app/(root)/banks/page.tsx
@@ -0,0 +1,3 @@
+export default function Page(): JSX.Element {
+  return <div className="p-4">Banks</div>;
+}
--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,22 +1,27 @@
 import { AmountChart } from "@/components/home/amountChart";
-import { MoneyAmount } from "@/components/home/moneyAmount";
+import { BalanceInfo } from "@/components/home/balanceInfo";
+import RightSideBar from "@/components/home/rightSideBar";
 import Wrapper from "@/components/shared/wrapper";
+import { accountsData } from "@/lib/data";
 
 export default function Home() {
   return (
-    <Wrapper>
-      <header>
-        <h1 className="text-2xl font-bold">
-          Welcome <span className="text-blue-500">Adrian</span>
-        </h1>
-        <p className="mt-2">
-          Access and manage your account and transactions efficiently
-        </p>
-      </header>
-      <section className="flex mt-10 gap-2">
-        <AmountChart />
-        <MoneyAmount totalAccounts={1} totalAmount={1200} />
-      </section>
-    </Wrapper>
+    <div className="flex justify-between w-full ">
+      <Wrapper>
+        <header className="w-full">
+          <h1 className="text-2xl font-bold">
+            Welcome <span className="text-blue-500">Adrian</span>
+          </h1>
+          <p className="mt-2">
+            Access and manage your account and transactions efficiently
+          </p>
+        </header>
+        <section className="flex mt-10 gap-2 sm:gap-4 w-full items-center p-4 shadow-container rounded-md">
+          <AmountChart data={accountsData} />
+          <BalanceInfo totalAccounts={1} totalAmount={1235.12} />
+        </section>
+      </Wrapper>
+      <RightSideBar />
+    </div>
   );
 }
