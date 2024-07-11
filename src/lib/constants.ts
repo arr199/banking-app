3e91708170628af336b5708f8c1371ed734cc78c
arr199 <abieledelso@gmail.com>
2024-07-11 19:43:18 +0100

    feat: Add navigation links to constants file

---
+++ src/lib/constants.ts
@@ -0,0 +1,28 @@
+export const navigationLinks = [
+    {
+      label: "home",
+      route: "/",
+      icon: "/icons/home.svg",
+    },
+  
+    {
+      label: "my banks",
+      route: "/banks",
+      icon: "/icons/dollar-circle.svg",
+    },
+    {
+      label: "transaction history",
+      route: "/transactions",
+      icon: "/icons/transaction.svg",
+    },
+    {
+      label: "payments transfer",
+      route: "/payments",
+      icon: "/icons/payment-transfer.svg",
+    },
+    {
+      label: "connect banks",
+      route: "/connect-banks",
+      icon: "/icons/connect-bank.svg",
+    },
+  ];
